77134e19dbc40aecf3ec90d21230165b95e1f2e6
Fred Durham <f.durham@protonmail.com>
2023-09-12 16:50:05 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Steps for Local Install
+1) npm install
+2) npm install -g ts-node typescript
---
+++ index.ts
@@ -0,0 +1,5 @@
+
+git remote add origin https://github.com/Team1-2308-Capstone/week4-project.git
+git branch -M main
+git push -u origin main
+
